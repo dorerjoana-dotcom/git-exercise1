4f965eb6913283000326ed64f29c74390760cc07
dorerjoana <dorer.joana@gmail.com>
2026-03-12 09:49:18 +0100

    init

---
+++ src/main/java/Add2Commits.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Add2Commits {
+
+    private static Object System;
+
+    static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello!");
+    }
+
+}
